d937bf700977dfac92611d82f9933cfa1a9280d8
Eazybel <eazybel27@gmail.com>
2026-01-13 16:25:21 -0900

    fix: update webhook URL for form submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ addSkillBtn.onclick=()=>{
 generateBtn.onclick=(e)=>{
     e.preventDefault()
     form.append("skills",skillContainer.innerText)
-     fetch("https://n8n-7hzw.onrender.com/webhook-test/5c444eef-efe7-4c9b-bc47-a4f4eea4ed23",{
+     fetch("https://n8n-7hzw.onrender.com/webhook/5c444eef-efe7-4c9b-bc47-a4f4eea4ed23",{
         method:"POST",
         header:{"Content-type":"application/json"},
         body:form
