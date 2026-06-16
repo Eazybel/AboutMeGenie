4f9d5972ad02b91bc3e197642b6c62471c97a777
Eazybel <131526601+Eazybel@users.noreply.github.com>
2026-06-17 00:39:46 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ const form=new FormData(formDatas)
 
 
 // When "Generate" is clicked, send user input to the webhook
-generateBtn.oonSubmit=async(e)=>{
+generateBtn.onClick=async(e)=>{
     loadingSpinner.classList.remove("hidden") // show spinner
     e.preventDefault()
     const payload = {
