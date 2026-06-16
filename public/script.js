717d913f7e59fb739b86d521d21c625485d51000
Eazybel <131526601+Eazybel@users.noreply.github.com>
2026-06-17 00:37:23 +0300

    Fix typo in event handler assignment for generateBtn

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ const form=new FormData(formDatas)
 
 
 // When "Generate" is clicked, send user input to the webhook
-formDatas.oonSubmit=async(e)=>{
+generateBtn.oonSubmit=async(e)=>{
     loadingSpinner.classList.remove("hidden") // show spinner
     e.preventDefault()
     const payload = {
